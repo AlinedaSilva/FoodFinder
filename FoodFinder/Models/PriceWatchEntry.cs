8241588577ba94fe0bff99e29fbe1d3d921eca9c
AlinedaSilva <35935280+AlinedaSilva@users.noreply.github.com>
2018-11-20 21:24:27 +0000

    Adding PriceWatchEntry

---
+++ FoodFinder/Models/PriceWatchEntry.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FoodFinder.Models
+{
+    public enum PriceIndicator
+    {
+        Up, Down, Same
+    }
+    public class PriceWatchEntry
+    {
+        public long Id { get; set; } // id for PriceWatchEntry
+        public DateTime Date { get; set; }
+        public decimal Price { get; set; }
+        public PriceIndicator PriceIndicator { get; set; }
+    }
+}
